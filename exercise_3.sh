9410f884adba99f7fa318910f8c3a58dc5d3d192
Ryan <mailforabe7@yahoo.com>
2021-03-16 14:04:05 +0200

    Exercise 3 with loop now

--- exercise_3.sh
+++ exercise_3.sh
@@ -1,1 +1,23 @@
-#This is the third
+#This is the third exercise
+
+dateformat=$(date +%F)
+echo "The date is $dateformat."
+
+#Move into test_pics folder
+cd test_pics/
+
+#Use loop to rename files
+#Just echoing them for now
+for NAME in $(ls); do
+
+if [ "$(file $NAME|grep jpg)" ]; then
+
+echo "Renaming ${NAME} to ${NAME%.*}.jpg"
+#mv ${NAME} ${NAME%.*}.gif
+
+else
+
+echo "No Change For ${NAME}"
+
+fi
+done
